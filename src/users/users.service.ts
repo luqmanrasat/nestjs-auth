3a1ec01b4a4febb1078fd4eef54fac34877271ae
Luqman Rasat <luqmanrasat@goodpeople.my>
2022-07-18 14:11:23 +0800

    create users module & mock users

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'user1',
+      username: 'username1',
+      password: 'pass1',
+    },
+    {
+      id: 2,
+      name: 'user2',
+      username: 'username2',
+      password: 'pass2',
+    },
+    {
+      id: 3,
+      name: 'user3',
+      username: 'username3',
+      password: 'pass3',
+    },
+  ];
+}
